093bf4957941c9167af7d82b50332a8608909b82
Mikhail Kos <him182@yandex.ru>
2024-05-29 12:31:04 +0300

    The project is the two step, file gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
-import { program } from "commander";
+import { program } from 'commander';
 
 program
 .name('gendiff')
 .description('Compares two configuration files and shows a difference.')
-.version('0.8.0');
+.version('0.8.0')
+.argument('<filepath1>')
+.argument('<filepath2>')
+.option('-f, --format [type]', 'output format');
 
 program.parse();
 
