7ed59cdc59824083b33f02747c4cb5d556ca94a3
Mikhail Kos <him182@yandex.ru>
2024-06-05 22:50:51 +0300

    test no lodas

--- Proba.js
+++ Proba.js
@@ -1,12 +1,12 @@
 import fs from 'node:fs';
-import _ from 'lodash';
+// import _ from 'lodash';
 
 const showeTheDiffer = (fileJson1, fileJsom2) => {
   let result = '';
   const file1 = JSON.parse(fs.readFileSync(fileJson1, 'utf8'));
   const file2 = JSON.parse(fs.readFileSync(fileJsom2, 'utf8'));
   const keySort = Object.keys({ ...file1, ...file2 });
-  const key = _.sortBy(keySort);
+  const key = keySort.sort();
   for (const keys of key) {
     const val1 = file1[keys];
     const val2 = file2[keys];
@@ -25,3 +25,5 @@ const showeTheDiffer = (fileJson1, fileJsom2) => {
 showeTheDiffer('file1.json', 'file2.json')
 
 export default showeTheDiffer;
+
+// const key = _.sortBy(keySort);
