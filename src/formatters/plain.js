6f32233cc467c8acd938df5d59d0882d79346c60
Mikhail Kos <him182@yandex.ru>
2024-07-04 13:39:25 +0300

    fix2

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -14,16 +14,16 @@ export default (data) => {
       const pathAcc = [...acc, node.name];
       const actualAcc = pathAcc.join('.');
       if (node.type === 'added') {
-        return `Property '${actualAcc}' WAS ADDED WITH VALUE: ${getValue(node.value)}`;
+        return `Property '${actualAcc}' was added with value: ${getValue(node.value)}`;
       }
       if (node.type === 'deleted') {
-        return `Property '${actualAcc}' WAS REMOVED`;
+        return `Property '${actualAcc}' was removed`;
       }
       if (node.type === 'unchanged') {
         return null;
       }
       if (node.type === 'changed') {
-        return `Property '${actualAcc}' WAS UPDATED. FROM ${getValue(node.oldValue)} TO ${getValue(node.newValue)}`;
+        return `Property '${actualAcc}' was updated. from ${getValue(node.oldValue)} to ${getValue(node.newValue)}`;
       }
       if (node.type === 'nested') {
         return `${iter(node.children, pathAcc)}`;
